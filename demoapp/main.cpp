e3fda7538a7daf211a39c77b08220d6a24daca50
mallard <mallard@14e2d357-2ffa-cf4b-9553-70cd0d08bd16>
2010-06-18 11:22:06 +0000

    Updated demo app
    Former-commit-id: 53b331a2a9916ecff4ed083fc49a76e0944a3e5c

--- demoapp/main.cpp
+++ demoapp/main.cpp
@@ -19,5 +19,10 @@ int main()
     hi+="hi!";
     mkv["set"].Call(make_tuple(string("hi"),hi));
     mkv["save"].Call(make_tuple(string("demofile")));
+    MetaClass gfx=conn.GetClass("gdemo", "gfx");
+    MetaObject mgfx=New(gfx);
+    mgfx["line"].Call(make_tuple(0,0,640,480,0xFFFF));
+    mgfx["line"].Call(make_tuple(640,0,0,480,0xFFFF));
+    while(true){sleep(1);}
     return 0;
 }
