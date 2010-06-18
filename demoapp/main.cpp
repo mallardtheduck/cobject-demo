95e9fb9415b958053c89576d6ba3474f4c10fcd6
mallard <mallard@14e2d357-2ffa-cf4b-9553-70cd0d08bd16>
2010-06-18 10:04:58 +0000

    Demo application
    Former-commit-id: 452fc6144d2589ed164efc1683acf530df337743

---
+++ demoapp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <meta.hpp>
+#include <connection.hpp>
+
+using namespace std;
+using namespace meta;
+using namespace boost;
+
+int main()
+{
+    cobject::Connection conn;
+    conn.SetNamespace("demoapp");
+    MetaClass kv=conn.GetClass("kvdemo", "KeyVal");
+    MetaObject mkv=New(kv);
+    mkv["load"].Call(make_tuple(string("demofile")));
+    mkv["set"].Call(make_tuple(string("foo"), string("bar")));
+    string hi=mkv["get"].Call<string>(make_tuple(string("hi")));
+    hi+="hi!";
+    mkv["set"].Call(make_tuple(string("hi"),hi));
+    mkv["save"].Call(make_tuple(string("demofile")));
+    return 0;
+}
